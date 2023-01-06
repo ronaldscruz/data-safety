ded257215e603a86e822736d3e2c82029f23bf3a
Ronald S. Cruz <ronald.scruz16@gmail.com>
2023-01-05 23:08:33 -0300

    refactor: remove carousel to test lighthouse benchmark

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import DefaultLayout from "../layouts/DefaultLayout";
 
-import HomeCarousel from "../components/pages/index/HomeCarousel";
+// import HomeCarousel from "../components/pages/index/HomeCarousel";
 import ServicesCards from "../components/pages/index/ServicesCards";
 
 export default function Home() {
   return (
     <DefaultLayout>
-      <HomeCarousel />
+      {/* <HomeCarousel /> */}
       <ServicesCards />
     </DefaultLayout>
   );
