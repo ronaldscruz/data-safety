f978c060ee2880ad78a7ea4c0fb5001dde7f1ef5
Ronald S. Cruz <ronald.scruz16@gmail.com>
2023-01-22 13:50:26 -0300

    feat: contact section boilerplate

--- components/pages/index/Contact/index.jsx
+++ components/pages/index/Contact/index.jsx
@@ -1,7 +1,19 @@
 import * as S from "./styles";
 
 const Contact = () => {
-  return <S.Container>Contact</S.Container>;
+  return (
+    <S.Container>
+      <S.Content>
+        <S.LeftContent>
+          <h1>Entre em contato</h1>
+          <h2>
+            Fale com nossa equipe de especialistas em segurança da informação
+          </h2>
+        </S.LeftContent>
+        <S.FormContainer>form goes here 8-)</S.FormContainer>
+      </S.Content>
+    </S.Container>
+  );
 };
 
 export default Contact;
--- components/pages/index/Contact/styles.js
+++ components/pages/index/Contact/styles.js
@@ -1,3 +1,30 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+
+  background: linear-gradient(
+    90deg,
+    var(--secondary-color) 0%,
+    var(--primary-color) 100%
+  );
+
+  height: 400px;
+  color: var(--negative-color);
+`;
+
+export const Content = styled.div`
+  width: var(--desktop-sm-w);
+  display: flex;
+  justify-content: space-between;
+  padding: var(--spacing-section-y) var(--spacing-section-x);
+`;
+
+export const LeftContent = styled.div`
+  width: 50%;
+`;
+
+export const FormContainer = styled.div`
+  width: 50%;
+`;
